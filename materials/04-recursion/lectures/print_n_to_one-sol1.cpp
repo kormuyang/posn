1113b06a804b431364f18010d8489a92df01f65f
kormuyang-lab <Phantakan04@gmail.com>
2025-07-13 21:54:48 +0700

    update recursion

--- materials/04-recursion/lectures/print_n_to_one-sol1.cpp
+++ materials/04-recursion/lectures/print_n_to_one-sol1.cpp
@@ -2,29 +2,18 @@
 
 using namespace std;
 
-// Solution 1
-void printOneToN(int n)
+void printNToOne(int n)
 {
     if (n == 0)
         return;
-    printOneToN(n - 1);
     cout << n << ' ';
+    printOneToN(n - 1);
     return;
 }
-
-// Solution 2
-// void printOneToN(int n, int i = 1)
-// {
-//     cout << i << ' ';
-//     if (i == n)
-//         return;
-//     printOneToN(n, i + 1);
-//     return;
-// }
 
 int main()
 {
     int n = 5;
-    printOneToN(n);
+    printNToOne(n);
     return 0;
 }
--- materials/04-recursion/lectures/print_one_to_n-sol1.cpp
+++ materials/04-recursion/lectures/print_one_to_n-sol1.cpp
@@ -2,18 +2,29 @@
 
 using namespace std;
 
-void printNToOne(int n)
+// Solution 1
+void printOneToN(int n)
 {
     if (n == 0)
         return;
-    cout << n << ' ';
     printOneToN(n - 1);
+    cout << n << ' ';
     return;
 }
+
+// Solution 2
+// void printOneToN(int n, int i = 1)
+// {
+//     cout << i << ' ';
+//     if (i == n)
+//         return;
+//     printOneToN(n, i + 1);
+//     return;
+// }
 
 int main()
 {
     int n = 5;
-    printNToOne(n);
+    printOneToN(n);
     return 0;
 }
